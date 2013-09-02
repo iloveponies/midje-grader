0ae8a966e31f9b31aeeff69e07384cc0e75e2eaa
Jani Rahkola <jani.rahkola@iki.fi>
2013-09-03 00:06:23 +0300

    init

---
+++ README.md
@@ -0,0 +1,1 @@
+# midje-grader
---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject midje-grader "0.1.0-SNAPSHOT"
+  :description ""
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [midje "1.5.1"]]
+  :profiles {:dev
+             {:plugins [[lein-midje "3.0.0"]]}})
