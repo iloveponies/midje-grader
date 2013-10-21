da704bb38ef6d9192fdfa584601d3965a126a308
Jani Rahkola <jani.rahkola@iki.fi>
2013-10-21 17:04:24 +0300

    includes a json version of the grading

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject midje-grader "0.1.0-SNAPSHOT"
   :description "Midje test runner that counts points based on meta data."
   :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.clojure/data.json "0.2.3"]
                  [midje "1.5.1"]]
   :profiles {:dev
              {:plugins [[lein-midje "3.0.0"]]}})
--- src/midje_grader.clj
+++ src/midje_grader.clj
@@ -2,7 +2,8 @@
   (:require [midje.emission.plugins.util :as util]
             [midje.emission.plugins.silence :as silence]
             [midje.emission.state :as state]
-            [clojure.pprint :as pp]))
+            [clojure.pprint :as pp]
+            [clojure.data.json :as json]))
 
 (def passed? (atom true))
 (def total-points (atom {}))
@@ -25,20 +26,27 @@
       (swap! earned-points #(assoc % exercise (+ got (get % exercise 0))))
       (util/emit-one-line (format "%d/%d points" got points)))))
 
+(defn before-all []
+  (swap! total-points (constantly {}))
+  (swap! earned-points (constantly {})))
+
 (defn after-all
   ([])
   ([_ _]
-     (pp/print-table
-      (sort-by :exercise
-               (for [[exercise got] @earned-points
-                     :let [out-of (get @total-points exercise)]]
-                 {"exercise" exercise
-                  "got" got
-                  "out-of" out-of})))))
+     (let [data (for [[exercise got] @earned-points
+                      :let [out-of (get @total-points exercise)]]
+                  {"exercise" exercise
+                   "got" got
+                   "out-of" out-of})]
+       (util/emit-one-line "midje-grader:data")
+       (util/emit-one-line (json/write-str data))
+       (util/emit-one-line "midje-grader:data")
+       (pp/print-table data))))
 
 (state/install-emission-map
  (assoc silence/emission-map
    :fail when-fail
    :starting-to-check-fact before-fact
    :finishing-fact after-fact
+   :starting-fact-stream before-all
    :finishing-fact-stream after-all))
