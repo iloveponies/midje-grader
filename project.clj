a48bf24eea5ccee8c0d99aee947dff3d5342202e
Jani Rahkola <jani.rahkola@iki.fi>
2013-11-28 15:45:42 +0200

    project url and license

--- project.clj
+++ project.clj
@@ -1,5 +1,8 @@
 (defproject midje-grader "0.1.0-SNAPSHOT"
   :description "Midje test runner that counts points based on meta data."
+  :url "http://github.com/iloveponies/midje-grader"
+  :license {:name "MIT License"
+            :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/data.json "0.2.3"]
                  [midje "1.5.1"]]
